0b6314cf052c54633b87beb5cdb11b0706b1c66f
Meang27 <54785631+Meang27@users.noreply.github.com>
2022-04-14 00:42:05 +0900

    Add files via upload
    Vuforia Script

---
+++ Script/Click.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Click : MonoBehaviour
+{
+    public GameObject map;
+    public VirtualButtonBehaviour Vb;
+
+    // Use this for initialization
+    void Start()
+    {
+        //map = GameObject.Find("Map");
+        Vb.RegisterOnButtonPressed(OnButtonPressed);
+        Vb.RegisterOnButtonReleased(OnButtonReleased);
+
+        map.SetActive(false);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        map.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        map.SetActive(false);
+    }
+}
